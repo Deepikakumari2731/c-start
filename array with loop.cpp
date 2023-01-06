e05fa07881a86190271bf503a4178200372b829a
Deep2731 <118149890+Deepikakumari2731@users.noreply.github.com>
2023-01-06 19:03:26 +0530

    Add files via upload

---
+++ array with loop.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int marks[]{10,20,30,40,50,60,70,80,90,100};
+	for(int i=0;i<11;i++){
+		cout<<"The marks of  "<<i<<"  is  "<<marks[i]<<endl;
+	}
+	return 0;
+}
